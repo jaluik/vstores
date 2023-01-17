f5d6444667fa20904b9cf2483605a6e7e5cb6476
jaluik <cqzhengmouren@gmail.com>
2023-01-17 14:54:36 +0800

    feat: 暴露适配器的配置

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import Vstore from './core/Vstore';
+export * as webAdaper from './adapter/web';
+export * as wxAdapter from './adapter/wx';
+export * as aliAdapter from './adapter/ali';
 
 const vstore = new Vstore();
 
